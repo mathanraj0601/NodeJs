67c6cbc8bf01fc9ae3ce6f8c9752e3c32b87d2f0
Mathanraj T <98396468+mathanraj0601@users.noreply.github.com>
2024-01-24 16:36:11 +0530

    Update event-emitter.js

--- node-demo/event-emitter.js
+++ node-demo/event-emitter.js
@@ -2,8 +2,8 @@ const EventEmiiter = require('events'); // return a class
 
 /* 
 event object has only track of event emitted from it and not from other modules. 
-so we cab't emit from one modulw and cathc it in other.so best pratice is to extend
- the event emitter class.
+so we cab't emit from one module and catch it in other because of multiple instance of emitter.so best pratice is to extend
+ the event emitter class to have a single instance across module to overcome above.
  */
 
 
