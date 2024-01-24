d53dac62a307f8670eeadf74acae10e9f87b7d94
Mathanraj T <98396468+mathanraj0601@users.noreply.github.com>
2024-01-24 16:45:47 +0530

    Update index.js

--- publish-demo/index.js
+++ publish-demo/index.js
@@ -1,6 +1,6 @@
 
-// initial version
+// initial version using npm publish
 module.exports.v1 = ()=>console.log('v1');
 
-// updated version
+// initial version using npm publish with change in package.json file
 module.exports.v2 = ()=>console.log('v2');
